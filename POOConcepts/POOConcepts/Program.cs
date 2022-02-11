1d85918ba64d1ed7de9593bba1b7c0d2c0b3f89a
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-02-10 21:44:36 -0500

    Finish date class

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 10));
+    Console.WriteLine(new Date(1974, 12, 23));
+    Console.WriteLine(new Date(2023, 11, 9));
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
